2ed6a8b57cffd7c80f4afeb04fe8a93bdda52c1f
SMHRD <SMHRD@222.102.104.70>
2023-01-05 10:06:11 +0900

    메이븐 환경 구축

---
+++ FOODLE/src/main/java/com/smhrd/controller/LoginCon.java
@@ -0,0 +1,30 @@
+package com.smhrd.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginCon extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("utf-8");
+		
+		String id = request.getParameter("ID"); // 값 받아오기
+		String pw = request.getParameter("PW");
+		
+		System.out.println(id + pw);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ FOODLE/src/main/java/com/smhrd/model/VO.java
@@ -0,0 +1,7 @@
+package com.smhrd.model;
+
+public class VO {
+	
+	
+
+}
