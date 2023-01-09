73c4fa63cca077ad47784ddd0dda381d15fc1cc7
smhrd <smhrd@222.102.104.190>
2023-01-09 16:54:40 +0900

    2023/01/09 진행과정

--- FOODLE/src/main/java/com/smhrd/model/BwChartDAO.java
+++ FOODLE/src/main/java/com/smhrd/model/BwChartDAO.java
@@ -11,11 +11,26 @@ public class BwChartDAO {
 	// 아래 메소드들에서 사용할 수 있는 sqlSession 생성
 	SqlSession sqlSession = sqlSessionFactory.openSession(true);
 	
-	public void todayChart() {
+	public String todayChart(String vo) {
+		String todayChart = null;
 		
-		
+		try {
+			todayChart = sqlSession.selectOne("com.smhrd.BwChartDAO.todayChart", vo);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			sqlSession.close();
+		}
+		return todayChart;
 		
 	}
+	
+	public void userChart() {
+		
+	}
+	
+	
 	
 	
 	
