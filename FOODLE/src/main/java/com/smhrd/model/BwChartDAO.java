5c1e3794ad0950a240f3a6cce05a7c164b2b0555
smhrd <smhrd@222.102.104.190>
2023-01-09 15:07:08 +0900

    2023 / 01 / 09 yg,bw

---
+++ FOODLE/src/main/java/com/smhrd/model/BwChartDAO.java
@@ -0,0 +1,23 @@
+package com.smhrd.model;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.smhrd.database.SqlSessionManager;
+
+public class BwChartDAO {
+	
+	SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSession();
+	// 아래 메소드들에서 사용할 수 있는 sqlSession 생성
+	SqlSession sqlSession = sqlSessionFactory.openSession(true);
+	
+	public void todayChart() {
+		
+		
+		
+	}
+	
+	
+	
+	
+}
---
+++ FOODLE/src/main/java/com/smhrd/model/YgChartVO.java
@@ -0,0 +1,33 @@
+package com.smhrd.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@AllArgsConstructor
+@RequiredArgsConstructor // final or @NonNull 파라미터만 받는 생성자
+@NoArgsConstructor
+@Getter // getter메
+public class YgChartVO {
+		
+		@NonNull
+		private String f_name;
+		@NonNull
+		private String f_gram;
+		private String f_kcal;
+		private String f_ch;
+		private String f_pro;
+		private String f_fat;
+		private String f_sugar;
+		private String f_sodium;
+		private String f_col;
+		private String f_fad;
+		private String f_trans;
+		
+		
+		
+	}
+
+
